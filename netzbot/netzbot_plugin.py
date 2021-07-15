fec5bcb1e6199d6434d2c840b844b93a1e70d8d8
Hendrik Fritsch <hendrik.fritsch@web.de>
2021-07-15 11:16:00 +0200

    Remove easter eggs and fix optional !netz parameter

--- netzbot/netzbot_plugin.py
+++ netzbot/netzbot_plugin.py
@@ -1,6 +1,8 @@
 from irc3.plugins.command import command
 import irc3
 
+with open('sl.txt') as f:
+    sl = f.read().split('\n')
 
 @irc3.plugin
 class Plugin:
@@ -19,7 +21,7 @@ class Plugin:
         if netz is not None:
             netz = ' '.join(netz)
         self.bot.db.set(mask.nick, netz=netz)
-        yield 'ok'
+        yield 'ack'
 
     @command(permission='view')
     def netz(self, mask, target, args):
@@ -28,10 +30,12 @@ class Plugin:
             %%netz [<nick>]
         """
         print('{mask} netz {args}'.format(mask=mask, args=args))
-        nick = args.get('<nick>', mask.nick)
+        nick = args.get('<nick>', None)
+        if nick is None:
+            nick = mask.nick
         yield self.bot.db.get(nick, {}).get('netz', None) or 'Nichts bekannt über {nick}'.format(nick=nick)
 
-    @command(permission='admin')
+    @command(permission='admin', show_in_help_list=False)
     def netzoverride(self, mask, target, args):
         """Netz überschreiben
 
@@ -42,7 +46,7 @@ class Plugin:
         if netz is not None:
             netz = ' '.join(netz)
         self.bot.db.set(args['<nick>'], netz=netz)
-        yield 'ok'
+        yield 'ack'
 
     @command(permission='view')
     def netzinfo(self, mask, target, args):
@@ -51,5 +55,16 @@ class Plugin:
             %%netzinfo
         """
         print('{mask} netzinfo {args}'.format(mask=mask, args=args))
-        yield 'Nutzer können mit !meinnetz [<Netz>...] ihr Netz festlegen. Mit !netz [<Nick>] kann es abgefragt werden. Admin ist h3ndr1k.'
+        yield 'Nutzer können mit !meinnetz [<Netz>...] ihr Netz festlegen. Mit !netz [<Nick>] kann es abgefragt werden.'
+
+    @command(permission='view')
+    def sl(self, mask, target, args):
+        """list directory contents
+
+            %%sl [<options>...]
+        """
+        print('{mask} sl {args}'.format(mask=mask, args=args))
+        yield 'CHOO CHOO!'
+        for line in sl:
+            self.bot.privmsg(mask.nick, line)
 
