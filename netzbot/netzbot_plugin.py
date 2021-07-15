600dd4f6012aafe28b3dbdc3624a9c36339e88bd
Hendrik Fritsch <hendrik.fritsch@web.de>
2021-07-15 16:04:13 +0200

    Add !allenetze and systemd service

--- netzbot/netzbot_plugin.py
+++ netzbot/netzbot_plugin.py
@@ -1,14 +1,18 @@
+import unicodedata
+import itertools
+import re
+
 from irc3.plugins.command import command
 import irc3
 
-with open('sl.txt') as f:
-    sl = f.read().split('\n')
-
 @irc3.plugin
 class Plugin:
-
     def __init__(self, bot):
         self.bot = bot
+        with open('sl.txt') as f:
+            self.sl = f.read().split('\n')
+        # https://modern.ircdocs.horse/formatting.html
+        self.color_re = re.compile('\x03(\d\d?(,\d\d?)?)?|\x04([0-9a-f]{6}(,[0-9a-f]{6})?)?', re.ASCII)
 
     @command(permission='view')
     def meinnetz(self, mask, target, args):
@@ -30,7 +34,7 @@ class Plugin:
             %%netz [<nick>]
         """
         print('{mask} netz {args}'.format(mask=mask, args=args))
-        nick = args.get('<nick>', None)
+        nick = args['<nick>']
         if nick is None:
             nick = mask.nick
         yield self.bot.db.get(nick, {}).get('netz', None) or 'Nichts bekannt über {nick}'.format(nick=nick)
@@ -57,6 +61,28 @@ class Plugin:
         print('{mask} netzinfo {args}'.format(mask=mask, args=args))
         yield 'Nutzer können mit !meinnetz [<Netz>...] ihr Netz festlegen. Mit !netz [<Nick>] kann es abgefragt werden.'
 
+    def clean_netz(self, netz):
+        """Remove colors and control characters"""
+        netz = self.color_re.sub('', netz)
+        return ''.join([c for c in netz if unicodedata.category(c) != 'Cc'])
+
+    @command(permission='view')
+    def allenetze(self, mask, target, args):
+        """Zeigt alle eingetragenen Netze an
+
+            %%allenetze
+        """
+        print('{mask} allenetze {args}'.format(mask=mask, args=args))
+        # uncool hack (.backend.db), as there is no official way to enumerate first level keys
+        netze = [self.clean_netz(obj['netz']) for obj in self.bot.db.backend.db.values() if obj.get('netz', None) is not None]
+        sizemap = {}
+        for key, group in itertools.groupby(netze):
+            # unsorted, so will get multiple calls for one key
+            s = sizemap.get(key, 0)
+            sizemap[key] = s + len(list(group))
+        output = sorted(sizemap.items(), key=lambda t: '{0} {1}'.format(t[1], t[0].lower()), reverse=True)
+        yield ', '.join(['{0[0]}: {0[1]}'.format(t) for t in output])
+
     @command(permission='view')
     def sl(self, mask, target, args):
         """list directory contents
@@ -65,7 +91,7 @@ class Plugin:
         """
         print('{mask} sl {args}'.format(mask=mask, args=args))
         yield 'CHOO CHOO!'
-        for line in sl:
+        for line in self.sl:
             self.bot.privmsg(mask.nick, line)
 
     @command(permission='view', show_in_help_list=False)
